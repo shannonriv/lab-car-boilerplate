616999505c15cef53edf34d343d58a9c1ae375b2
shannonriv <shannonriv18@gmail.com>
2017-12-18 01:11:21 -0500

    cargando version mobile y desktop

--- js/app.js
+++ js/app.js
@@ -1,1 +1,47 @@
-// La funcionalidad de tu proyecto
+$(document).ready(function(){
+  $("#myBtn").click(function(){
+      $("#myModal").modal();
+  });
+});
+
+$(document).ready(function(){
+  $("#myBtn2").click(function(){
+      $("#myModal2").modal();
+  });
+});
+
+$(document).ready(function(){
+  $("#myBtn3").click(function(){
+      $("#myModal3").modal();
+  });
+});
+
+function initMap() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: {lat: -12.026733806103568, lng: -76.98777915}
+  });
+  directionsDisplay.setMap(map);
+
+  var onChangeHandler = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('start').addEventListener('change', onChangeHandler);
+  document.getElementById('end').addEventListener('change', onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: document.getElementById('start').value,
+    destination: document.getElementById('end').value,
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
